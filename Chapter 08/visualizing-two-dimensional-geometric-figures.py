e148e7add86f3099f4138343973c2d76918d90d5
Rin Arakaki <rnarkkx@gmail.com>
2023-04-23 14:11:18 +0900

    Update visualizing-two-dimensional-geometric-figures.py

--- Chapter 08/visualizing-two-dimensional-geometric-figures.py
+++ Chapter 08/visualizing-two-dimensional-geometric-figures.py
@@ -1,4 +1,4 @@
-import numpy as np
+import jax.numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 from matplotlib.collections import PatchCollection
@@ -10,7 +10,6 @@ fig, ax = plt.subplots()
 outer = Circle((0.0, 0.0), 1.0, zorder=0, fc="k")
 ax.add_patch(outer)
 
-
 col = PatchCollection(
     (Circle((x, y), r) for x, y, r in data),
     facecolor="white", zorder=1, linewidth=0.2,
@@ -21,6 +20,5 @@ ax.add_collection(col)
 ax.set_xlim((-1.1, 1.1))
 ax.set_ylim((-1.1, 1.1))
 ax.set_axis_off()
-
 
 plt.show()
