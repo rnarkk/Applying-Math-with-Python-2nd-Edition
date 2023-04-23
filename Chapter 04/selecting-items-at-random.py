f932f6d725faa7c4e6d7a72fec53097c2c9ed67e
Rin Arakaki <rnarkkx@gmail.com>
2023-04-23 13:51:59 +0900

    Update selecting-items-at-random.py

--- Chapter 04/selecting-items-at-random.py
+++ Chapter 04/selecting-items-at-random.py
@@ -1,6 +1,7 @@
-import numpy as np
+import jax.numpy as np
+from jax.random import PRNGKey
 
-rng = np.random.default_rng(12345)
+rng = PRNGKey(12345)
 
 data = np.arange(15)
 probabilities = np.array([0.3, 0.2, 0.1, 0.05, 0.05, 0.05, 0.05, 0.025,
@@ -12,7 +13,7 @@ selected = rng.choice(data, p=probabilities, replace=True)
 # 0
 
 selected_array = rng.choice(data, p=probabilities, replace=True, size=(5, 5))
-#array([[ 1, 6, 4, 1, 1],
+# array([[ 1, 6, 4, 1, 1],
 #       [ 2, 0, 4, 12, 0],
 #       [12, 4, 0, 1, 10],
 #       [ 4, 1, 5, 0, 0],
