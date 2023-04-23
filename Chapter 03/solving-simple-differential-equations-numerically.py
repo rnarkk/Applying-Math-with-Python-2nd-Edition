2c3a0988eae3a57190a7b349ce2d0c72c436b851
Rin Arakaki <rnarkkx@gmail.com>
2023-04-23 13:45:35 +0900

    Update solving-simple-differential-equations-numerically.py

--- Chapter 03/solving-simple-differential-equations-numerically.py
+++ Chapter 03/solving-simple-differential-equations-numerically.py
@@ -1,16 +1,16 @@
-import numpy as np
-from scipy import integrate
+import jax.numpy as np
+from jax.scipy import integrate
 import matplotlib.pyplot as plt
 
 def f(t, y):
-    return -0.2*y
+    return -0.2 * y
 
 t_range = (0, 5)
 
 T0 = np.array([50.])
 
 def true_solution(t):
-    return 50.*np.exp(-0.2*t)
+    return 50. * np.exp(-0.2 * t)
 
 sol = integrate.solve_ivp(f, t_range, T0, max_step=0.1)
 
@@ -23,7 +23,6 @@ ax1.plot(t_vals, T_vals, "k")
 ax1.set_xlabel("$t$")
 ax1.set_ylabel("$T$")
 ax1.set_title("Solution of the cooling equation")
-
 
 err = np.abs(T_vals - true_solution(t_vals))
 ax2.semilogy(t_vals, err, "k")
