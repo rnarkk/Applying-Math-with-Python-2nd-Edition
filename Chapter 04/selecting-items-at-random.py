4aff10a3e3b7942269e397df57a05e8151afa890
root <rnarkkx@gmail.com>
2023-04-23 06:27:08 +0000

    Update

--- Chapter 04/selecting-items-at-random.py
+++ Chapter 04/selecting-items-at-random.py
@@ -1,18 +1,18 @@
+from jax import random
 import jax.numpy as np
-from jax.random import PRNGKey
 
-rng = PRNGKey(12345)
+key = random.PRNGKey(12345)
 
 data = np.arange(15)
 probabilities = np.array([0.3, 0.2, 0.1, 0.05, 0.05, 0.05, 0.05, 0.025,
                           0.025, 0.025, 0.025, 0.025, 0.025, 0.025, 0.025])
 
-assert round(sum(probabilities), 10) == 1.0, "Probabilities must sum to 1"
+assert np.round(np.sum(probabilities), 10) == 1.0, "Probabilities must sum to 1"
 
-selected = rng.choice(data, p=probabilities, replace=True)
+selected = random.choice(key, data, p=probabilities, replace=True)
 # 0
 
-selected_array = rng.choice(data, p=probabilities, replace=True, size=(5, 5))
+selected_array = random.choice(key, data, (5, 5), p=probabilities, replace=True)
 # array([[ 1, 6, 4, 1, 1],
 #       [ 2, 0, 4, 12, 0],
 #       [12, 4, 0, 1, 10],
