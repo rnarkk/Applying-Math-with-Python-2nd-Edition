b70a2441f49f008185c7c8b88daa5cd900fa884c
root <rnarkkx@gmail.com>
2023-04-23 06:06:48 +0000

    Update

--- Chapter 01/decimal-type.py
+++ Chapter 01/decimal-type.py
@@ -7,15 +7,15 @@ num1 + num2  # Decimal('2.663')
 from decimal import getcontext
 ctx = getcontext()
 num = Decimal('1.1')
-num**4 # Decimal('1.4641')
+num ** 4 # Decimal('1.4641')
 ctx.prec = 4 # set new precision
-num**4 # Decimal('1.464')
+num ** 4 # Decimal('1.464')
 
 
 from decimal import localcontext
 num = Decimal("1.1")
 with localcontext() as ctx:
     ctx.prec = 2
-    num**4 # Decimal('1.5')
-num**4 # Decimal('1.4641')
+    num ** 4  # Decimal('1.5')
+num ** 4  # Decimal('1.4641')
 
