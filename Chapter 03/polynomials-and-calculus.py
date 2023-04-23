01e1c3b4461a1756894ae66f88c3454379264c09
Rin Arakaki <rnarkkx@gmail.com>
2023-04-23 13:43:34 +0900

    Update polynomials-and-calculus.py

--- Chapter 03/polynomials-and-calculus.py
+++ Chapter 03/polynomials-and-calculus.py
@@ -1,4 +1,3 @@
-
 class Polynomial:
     """Basic polynomial class"""
 
@@ -9,19 +8,18 @@ class Polynomial:
         return f"Polynomial({repr(self.coeffs)})"
 
     def __call__(self, x):
-        return sum(coeff*x**i for i, coeff in enumerate(self.coeffs))
+        return sum(coeff * x ** i for i, coeff in enumerate(self.coeffs))
 
     def differentiate(self):
         """Differentiate the polynomial and return the derivative"""
-        coeffs = [i*c for i, c in enumerate(self.coeffs[1:], start=1)]
+        coeffs = [i * c for i, c in enumerate(self.coeffs[1:], start=1)]
         return Polynomial(coeffs)
 
     def integrate(self, constant=0):
         """Integrate the polynomial and return the integral"""
         coeffs = [float(constant)]
-        coeffs += [c/i for i, c in enumerate(self.coeffs, start=1)]
+        coeffs += [c / i for i, c in enumerate(self.coeffs, start=1)]
         return Polynomial(coeffs)
-
 
 p = Polynomial([1, -2, 1])
 p.differentiate()
