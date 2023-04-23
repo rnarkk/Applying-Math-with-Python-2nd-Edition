29c0e05c441a1e09b38ab9ba789d5ea835f71a29
Rin Arakaki <rnarkkx@gmail.com>
2023-04-23 13:50:05 +0900

    Update generating-normally-distributed-random-numbers.py

--- Chapter 04/generating-normally-distributed-random-numbers.py
+++ Chapter 04/generating-normally-distributed-random-numbers.py
@@ -1,11 +1,11 @@
-import numpy as np
+import jax.numpy as np
 import matplotlib.pyplot as plt
 
-from numpy.random import default_rng
-rng = default_rng(12345)
+from jax.random import PRNGKey
+rng = PRNGKey(12345)
 
 mu = 5.0  # mean value
-sigma = 3.0 # standard deviation
+sigma = 3.0  # standard deviation
 rands = rng.normal(loc=mu, scale=sigma, size=10000)
 
 fig, ax = plt.subplots()
@@ -15,11 +15,10 @@ ax.set_xlabel("Value")
 ax.set_ylabel("Density")
 
 def normal_dist_curve(x):
-    return 10000*np.exp(-0.5*((x-mu)/sigma)**2)/(sigma*np.sqrt(2*np.pi))
+    return 10000 * np.exp(-0.5 * ((x - mu) / sigma) ** 2) / (sigma * np.sqrt(2 * np.pi))
 
 x_range = np.linspace(-5, 15)
 y = normal_dist_curve(x_range)
 ax.plot(x_range, y, "k--")
-
 
 plt.show()
