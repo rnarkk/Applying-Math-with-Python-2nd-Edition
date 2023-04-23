9271cda4395b7c1e621166ebf39744826e1e9e0a
Rin Arakaki <rnarkkx@gmail.com>
2023-04-23 13:30:54 +0900

    Update solving-equations.py

--- Chapter 01/solving-equations.py
+++ Chapter 01/solving-equations.py
@@ -1,16 +1,15 @@
-from scipy import optimize
-from math import exp
+import jax.numpy as np
+from jax.scipy import optimize
 
 def f(x):
-    return x*(x - 2)*exp(3 - x)
+    return x * (x - 2) * np.exp(3 - x)
 
 def fp(x):
-    return -(x**2 - 4*x + 2)*exp(3 - x)
+    return -(x ** 2 - 4 * x + 2) * np.exp(3 - x)
 
-
-optimize.newton(f, 1., x1=1.5) # Using x1 = 1.5 and the secant method
+optimize.newton(f, 1., x1=1.5)  # Using x1 = 1.5 and the secant method
 # 1.9999999999999862
-optimize.newton(f, 1., fprime=fp) # Using Newton-Raphson method
+optimize.newton(f, 1., fprime=fp)  # Using Newton-Raphson method
 # 2.0
 
 
