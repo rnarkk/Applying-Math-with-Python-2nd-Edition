2c2b6df7fbca0dbb4479ec8ee3ae1af744be6732
Rin Arakaki <rnarkkx@gmail.com>
2023-04-23 14:15:43 +0900

    Update using-gradient-descent-methods.py

--- Chapter 09/using-gradient-descent-methods.py
+++ Chapter 09/using-gradient-descent-methods.py
@@ -1,4 +1,4 @@
-import numpy as np
+import jax.numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -8,21 +8,18 @@ def descend(func, x0, grad, bounds, tol=1e-8, max_iter=100):
     grad_xn = grad(x0)
 
     for i in range(max_iter):
-
         if np.linalg.norm(xn - previous) < tol:
             break
 
         direction = -grad_xn
 
         previous = xn
-        xn = xn + 0.2*direction
+        xn = xn + 0.2 * direction
         grad_xn = grad(xn)
         yield i, xn, func(xn), grad_xn
 
-
 def func(x):
-    return ((x[0] - 0.5)**2 + (x[1] + 0.5)**2)*np.cos(0.5*x[0]*x[1])
-
+    return ((x[0] - 0.5) ** 2 + (x[1] + 0.5) ** 2) * np.cos(0.5*x[0]*x[1])
 
 x_r = np.linspace(-1, 1)
 y_r = np.linspace(-2, 2)
@@ -31,7 +28,6 @@ x, y = np.meshgrid(x_r, y_r)
 
 z = func([x, y])
 
-
 surf_fig = plt.figure(tight_layout=True)
 surf_ax = surf_fig.add_subplot(projection="3d")
 surf_ax.tick_params(axis="both", which="major", labelsize=9)
@@ -39,7 +35,6 @@ surf_ax.set(xlabel="x", ylabel="y", zlabel="z")
 surf_ax.set_title("Objective function")
 
 surf_ax.plot_surface(x, y, z, cmap="gray", vmax=8.0, alpha=0.5)
-
 
 x0 = np.array([-0.8, 1.3])
 surf_ax.plot([x0[0]], [x0[1]], func(x0), "k*")
@@ -52,7 +47,6 @@ def grad(x):
         (2*x[0]-1)*cos_t - 0.5*x[1]*c1*sin_t,
         (2*x[1]+1)*cos_t - 0.5*x[0]*c1*sin_t
     ])
-
 
 cont_fig, cont_ax = plt.subplots()
 cont_ax.set(xlabel="x", ylabel="y")
