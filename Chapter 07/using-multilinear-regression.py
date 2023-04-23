bf894c37e255fb5df860c4620180a964029c6c85
root <rnarkkx@gmail.com>
2023-04-23 07:19:12 +0000

    Update

--- Chapter 07/using-multilinear-regression.py
+++ Chapter 07/using-multilinear-regression.py
@@ -1,20 +1,20 @@
+from jax import random
 import jax.numpy as np
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 
-from jax.random import default_rng
-rng = default_rng(12345)
+key = random.PRNGKey(12345)
 
 p_vars = pd.DataFrame({
     "const": np.ones((100,)),
-    "X1": rng.uniform(0, 15, size=100),
-    "X2": rng.uniform(0, 25, size=100),
-    "X3": rng.uniform(5, 25, size=100)
+    "X1": random.uniform(key, (100,), minval=0, maxval=15),
+    "X2": random.uniform(key, (100,), minval=0, maxval=25),
+    "X3": random.uniform(key, (100,), minval=5, maxval=25)
 })
 
-residuals = rng.normal(0.0, 12.0, size=100)
-Y = -10.0 + 5.0*p_vars["X1"] - 2.0*p_vars["X2"] + residuals
+residuals = random.normal(key, (100,)) * 12.0
+Y = -10.0 + 5.0 * p_vars["X1"] - 2.0*p_vars["X2"] + residuals
 
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True, tight_layout=True)
 ax1.scatter(p_vars["X1"], Y, c="k")
