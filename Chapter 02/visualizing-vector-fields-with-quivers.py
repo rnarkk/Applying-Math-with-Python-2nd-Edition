89fc71049acfd779a3bc39bb647ce9e32eef47f2
Rin Arakaki <rnarkkx@gmail.com>
2023-04-23 13:37:04 +0900

    Update visualizing-vector-fields-with-quivers.py

--- Chapter 02/visualizing-vector-fields-with-quivers.py
+++ Chapter 02/visualizing-vector-fields-with-quivers.py
@@ -1,17 +1,14 @@
-import numpy as np
+import jax.numpy as np
 import matplotlib.pyplot as plt
 
-
 def f(x, y): 
-    v = x**2 + y**2
-    return np.exp(-2*v)*(x + y), np.exp(-2*v)*(x - y)
-
+    v = x ** 2 + y ** 2
+    return np.exp(-2 * v) * (x + y), np.exp(-2 * v) * (x - y)
 
 t = np.linspace(-1., 1.)
 x, y = np.meshgrid(t, t)
 
 dx, dy = f(x, y)
-
 
 fig, ax = plt.subplots()
 ax.quiver(x, y, dx, dy)
@@ -21,5 +18,3 @@ ax.set_ylabel("y")
 ax.set_title("Quiver plot of a vector field")
 
 plt.show()
-
-
