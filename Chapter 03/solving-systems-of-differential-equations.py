a9a571de7edc5c74ee9af211ffd5ab8b6531e25e
Rin Arakaki <rnarkkx@gmail.com>
2023-04-23 13:46:14 +0900

    Update solving-systems-of-differential-equations.py

--- Chapter 03/solving-systems-of-differential-equations.py
+++ Chapter 03/solving-systems-of-differential-equations.py
@@ -1,9 +1,8 @@
-import numpy as np
+import jax.numpy as np
 import matplotlib.pyplot as plt
 
 def predator_prey_system(t, y):
-    return np.array([5*y[0] - 0.1*y[0]*y[1], 0.1*y[1]*y[0] - 6*y[1]])
-
+    return np.array([5 * y[0] - 0.1 * y[0] * y[1], 0.1 * y[1] * y[0] - 6 * y[1]])
 
 p = np.linspace(0, 100, 25)
 w = np.linspace(0, 100, 25)
@@ -17,14 +16,13 @@ ax.set_title("Population dynamics for two competing species")
 ax.set_xlabel("P")
 ax.set_ylabel("W")
 
-
 initial_conditions = np.array([85, 40])
 
-from scipy import integrate
+from jax.scipy import integrate
+
 sol = integrate.solve_ivp(predator_prey_system, (0., 5.), initial_conditions, max_step=0.01)
 
 ax.plot(initial_conditions[0], initial_conditions[1], "ko")
 ax.plot(sol.y[0, :], sol.y[1, :], "k", linewidth=0.5)
 
 plt.show()
-
