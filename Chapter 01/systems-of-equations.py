af54294916f79b5a932c7757a05689ff1e63659f
Rin Arakaki <rnarkkx@gmail.com>
2023-04-23 13:32:14 +0900

    Update systems-of-equations.py

--- Chapter 01/systems-of-equations.py
+++ Chapter 01/systems-of-equations.py
@@ -1,9 +1,7 @@
-import numpy as np
-from numpy import linalg
+import jax.numpy as np
+from jax.numpy import linalg
 
 A = np.array([[3, -2, 1], [1, 1, -2], [-3, -2, 1]])
 b = np.array([7, -4, 1])
 
 linalg.solve(A, b)  # array([ 1., -1., 2.])
-
-
