6dc00f6497c2a92cff1776b4c652531570673531
root <rnarkkx@gmail.com>
2023-04-23 07:26:47 +0000

    Update

--- Chapter 03/discrete-fourier-transforms-for-signals-processing.py
+++ Chapter 03/discrete-fourier-transforms-for-signals-processing.py
@@ -1,14 +1,15 @@
 import jax.numpy as np
 from jax import random
 import matplotlib.pyplot as plt
-rng = random.PRNGKey(12345)
+
+key = random.PRNGKey(12345)
 
 def signal(t, freq_1=4.0, freq_2=7.0):
     return np.sin(freq_1 * 2 * np.pi * t) + np.sin(freq_2 * 2 * np.pi * t)
 
 sample_size = 2 ** 7
-sample_t = np.linspace(0, 4, sample_size, dtype=np.float64)
-sample_y = signal(sample_t) + rng.standard_normal(sample_size)
+sample_t = np.linspace(0, 4, sample_size, dtype=np.float32)
+sample_y = signal(sample_t) + random.normal(key, (sample_size,))
 sample_d = 4. / (sample_size - 1)  # Spacing for linspace array
 true_signal = signal(sample_t)
 
@@ -39,8 +40,8 @@ ax2.set_ylabel("Density")
 filtered = pos_freq_i[psd > 2e3]
 
 new_spec = np.zeros_like(spectrum)
-new_spec[filtered] = spectrum[filtered]
-new_spec[-filtered] = spectrum[-filtered]
+new_spec = new_spec.at[filtered].set(spectrum[filtered])
+new_spec = new_spec.at[-filtered].set(spectrum[-filtered])
 
 new_sample = np.real(fft.ifft(new_spec))
 
