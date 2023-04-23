14f888e328e3199b22aa7d2639fd1c45ddf85f76
Rin Arakaki <rnarkkx@gmail.com>
2023-04-23 13:28:40 +0900

    Update matrix-properties-of-numpy-arrays.py

--- Chapter 01/matrix-properties-of-numpy-arrays.py
+++ Chapter 01/matrix-properties-of-numpy-arrays.py
@@ -1,10 +1,9 @@
-import numpy as np
+import jax.numpy as np
 
 np.eye(3)
 # array([[1., 0., 0.],
 #        [0., 1., 0.],
 #        [0., 0., 1.]])
-
 
 mat = np.array([[1, 2], [3, 4]])
 mat.transpose()
@@ -13,7 +12,6 @@ mat.transpose()
 mat.T
 # array([[1, 3],
 #        [2, 4]])
-
 
 A = np.array([[1, 2], [3, 4]])
 A.trace()  # 5
@@ -27,20 +25,17 @@ A * B
 # array([[-1, 2],
 #        [ 0, 4]])
 
-
 A = np.array([[1, 2], [3, 4]])
 I = np.eye(2)
 A @ I
 # array([[1, 2],
 #        [3, 4]])
 
-
-from numpy import linalg
+from jax.numpy import linalg
 linalg.det(A)  # -2.0000000000000004
 linalg.inv(A)
 # array([[-2. , 1. ],
 #        [ 1.5, -0.5]])
-
 
 Ainv = linalg.inv(A)
 Ainv @ A
@@ -52,4 +47,3 @@ A @ Ainv
 # Approximately
 # array([[1., 0.],
 #        [0., 1.]])
-
